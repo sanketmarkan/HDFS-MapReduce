b9fc9a4d1a0fa0b62fd266edb3f25fc0c7ceed01
Sanket Markan <sanketmarkan@gmail.com>
2016-11-13 23:54:11 +0530

    BlockReport

--- DataNode.java
+++ DataNode.java
@@ -14,8 +14,10 @@ import java.rmi.registry.LocateRegistry;
 public class DataNode implements IDataNode {
 
 	private static int myId;
-	public static void main(String args[]){
+    private static ArrayList<Integer> blockList;
+    public static void main(String args[]){
         myId = Integer.parseInt(args[0]);
+        blockList = new ArrayList<Integer>();
 		init();
 	}
 
@@ -24,7 +26,7 @@ public class DataNode implements IDataNode {
             NameNode obj = new NameNode();
             INameNode stub = (INameNode) UnicastRemoteObject.exportObject(obj, 0);
             Registry registry = LocateRegistry.getRegistry();
-            registry.bind("datanode-"+myId, stub);
+            registry.bind("datanode", stub);
             System.out.println("DataNode " + myId +" ready");
         } catch (Exception e) {
             e.printStackTrace();
@@ -73,15 +75,32 @@ public class DataNode implements IDataNode {
 			response.setStatus(0);
 		}
 		else {
+            blockList.add(block);
 			response.setStatus(1);
 			// WRITE TO FILE BRO
 		}
 		return Utils.serialize(response.build());
 	}
 
+    public byte[] sendBlockReport() throws RemoteException {
+        BlockReportRequest.Builder request = BlockReportRequest.newBuilder();
+        request.setId(myId);
+        for(int block:blockList)
+            request.addBlockNumbers(block);
+        try{
+            Registry registry = LocateRegistry.getRegistry();
+            INameNode stub = (INameNode) registry.lookup("namenode");
+            stub.blockReport(Utils.serialize(request.build()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 	public void sendHeartBeat() throws RemoteException{
 		try{
-			Registry registry = LocateRegistry.getRegistry();
+			// Registry registry = LocateRegistry.getRegistry(nnIP, nnPort);
+            Registry registry = LocateRegistry.getRegistry();
 	        INameNode stub = (INameNode) registry.lookup("namenode");
 	        stub.heartBeat();
 	    } catch (Exception e) {
